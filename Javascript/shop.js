c9f3592e08b6bcf4b9f6477a11a0c229166b293e
GeorgesMarie <georgesmariemah@yahoo.fr>
2016-12-20 15:36:39 +0100

    html/css: OK

---
+++ Javascript/shop.js
@@ -0,0 +1,9 @@
+var initial = $(".shoes");
+	console.log(initial);
+	replace = $(".shoes_replace");
+	console.log(replace);
+
+$ (document).ready(function(){
+	console.log('Le dom est prêt !');
+
+})
