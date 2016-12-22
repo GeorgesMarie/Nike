f613351cff480e28ee3fa882888fb1d81f97e42d
GeorgesMarie <georgesmariemah@yahoo.fr>
2016-12-22 01:30:44 +0100

    je galère à faire fonctionner les fonctions

--- Javascript/shop.js
+++ Javascript/shop.js
@@ -1,9 +1,36 @@
-var initial = $(".shoes");
-	console.log(initial);
-	replace = $(".shoes_replace");
-	console.log(replace);
+console.log("Le dom est prêt !");
 
-$ (document).ready(function(){
-	console.log('Le dom est prêt !');
+			/* Déclaration de variables dans lesquelles je stocke
+					les classes que je vais manipuler */
 
-})
+var nikeG = document.getElementsByClassName('nikeG'),
+		nikeD = document.getElementsByClassName('nikeD'),
+		recup_shoes = document.getElementsByClassName('shoes'),
+		recup_shoesReplace = document.getElementsByClassName('shoes_replace');
+
+					/*Vérification des classes récupérées en les affichant dans la
+						console */
+console.log(nikeG);
+console.log(nikeD);
+console.log(recup_shoes);
+console.log(recup_shoesReplace);
+
+					/* Fonction qui change la propriété CSS quand on clique sur
+						la nike de droite */
+
+function masque(){
+		document.getElementsByClassName("shoes_replace").style.visibility=visible;
+		document.getElementsByClassName("shoes").style.visibility=hidden;
+};
+
+nikeD.addEventListener("click", masque);
+
+					/* Fonction qui change la propriété CSS quand on clique sur
+						la nike de gauche */
+
+function affiche(){
+		document.getElementsByClassName("shoes_replace").style.visibility=hidden;
+		document.getElementsByClassName("shoes").style.visibility=visible;
+};
+
+nikeG.addEventListener("click", affiche);
