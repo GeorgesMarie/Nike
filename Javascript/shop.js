e1c716e0b6f28e04a154b333dfe9aef29d9beb6d
GeorgesMarie <georgesmariemah@yahoo.fr>
2017-01-05 01:46:35 +0100

    Adds sounds

--- Javascript/shop.js
+++ Javascript/shop.js
@@ -1,41 +1,33 @@
-					/* Function that changes the CSS
-					property when you click
-					The right nike */
+					/* Function that changes central image  */
 $(document).ready(function(){
-		$(".nikeD").click(function(){
-			$(".shoes_replace").css("visibility", "visible");
-			$(".shoes").css("visibility", "hidden");
+			$('.nikeD').click(function(){
+			$('.shoes').attr('src', 'Images/nike_replace.png');
+			sound.play();
 		});
-});
-					/* Function that changes the CSS
-					property when you click
-					The left nike */
-$(document).ready(function(){
-		$(".nikeG").click(function(){
-			$(".shoes").css("visibility", "visible");
-			$(".shoes_replace").css("visibility", "hidden");
+
+		$('.nikeG').click(function(){
+			$('.shoes').attr('src', 'Images/nike.png');
+			sound.play();
 		});
-});
+
+		$('.nikeT').click(function(){
+			$('.shoes').attr('src', 'Images/nike_3.png');
+			sound.play();
+		});
+})
 
 					/* Now, try to create a click interaction on
 						The link ADD TO CART which will add 1 to the cart */
 $(document).ready(function(){
-			var $clicks = 0;
-				$(".add").click(function(){
-				$clicks++;
+			var	$clicks = 0;
+				$(".add, .add_2").click(function(){
+					$clicks++;
 				$(".ajout").html("(" + $clicks + ")");
+				beepOne.play();
 		});
 });
 
-$(document).ready(function(){
-			var $clicks = 0;
-				$(".add_2").click(function(){
-				$clicks++;
-				$(".ajout").html("(" + $clicks + ")");
-		});
-});
-
-					/* SWIPER for footer images */
+					/* SWIPER for images on footer */
 $(document).ready(function(){
 	var mySwiper = new Swiper('.swiper-container',{
 		pagination: '.pagination',
@@ -49,43 +41,65 @@ $(document).ready(function(){
 				/* Code of click on search
 					which displays a
 					Input */
-		$(document).ready(function(){
+	/*	$(document).ready(function(){
 			$("#loupe").click(function(){
 				$("#search").html("<input type='text' placeholder='Search'/>");
 			})
-		})
-
-				/* Code of the click on sign
-				 	in which displays a
-					form */
-$(document).ready(function(){
-		$("#bonhomme").click(function(){
-			$("#sign").html("<input type='text' placeholder='Votre nom'/><br><input type='password' placeholder='Votre mot de passe'/><br><button type='submit'>Login");
-		})
-})
+		})*/
 
 							/* Id of the #frame following the scroll*/
 $(document).ready(function(){
 	$(window).scroll(function(){
-
 		if ($(window).scrollTop() > 500 && $(window).scrollTop() < 1000)
-
 		$("#frame").css("position", "absolute").css("top", -624);
-
 	});
 });
 
 								/* Code that changes the heart when you click */
 $(document).ready(function(){
 				$(".heart").click(function(){
-				$(".heart_2").css("visibility", "visible");
-				$(".heart").css("visibility", "hidden");
+				$(".heart").attr("src", "Images/heart_2.png");
 	});
 });
 
 $(document).ready(function(){
-				$(".heart_2").click(function(){
-				$(".heart").css("visibility", "visible");
-				$(".heart_2").css("visibility", "hidden");
-	});
+				$("heart").click(function(){
+				$(".heart").attr("src", "Images/heart.png");
+		});
+});
+
+							/* Code that takes care of the form when you click on sign icon*/
+function deselect(e) {
+  $('.pop').slideFadeToggle(function() {
+    e.removeClass('selected');
+  });
+}
+
+$(function() {
+  $('#fellow').on('click', function() {
+		beep_beep.play();
+    if($(this).hasClass('selected')) {
+      deselect($(this));
+    } else {
+      $(this).addClass('selected');
+      $('.pop').slideFadeToggle();
+    }
+    return false;
+  });
+
+  $('.close').on('click', function() {
+    deselect($('#contact'));
+    return false;
+  });
+});
+
+$.fn.slideFadeToggle = function(easing, callback) {
+  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+};
+
+						/* Code js that plays sound at the time of the hover */
+$(document).ready(function(){
+			$(".options, #search, #cart, #sign").mouseenter(function(){
+			beep.play();
+		});
 });
