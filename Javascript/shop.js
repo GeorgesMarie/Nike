c98f40234a06996b76bcde77362fbfccc3e45724
GeorgesMarie <georgesmariemah@yahoo.fr>
2017-01-06 18:36:38 +0100

    First commit

--- Javascript/shop.js
+++ Javascript/shop.js
@@ -1,25 +1,25 @@
 					/* Function that changes central image  */
 $(document).ready(function(){
-			$('.nikeD').click(function(){
+			$('.nikeD').mouseenter(function(){
 			$('.shoes').attr('src', 'Images/nike_replace.png');
 			sound.play();
 		});
 
-		$('.nikeG').click(function(){
+		$('.nikeG').mouseenter(function(){
 			$('.shoes').attr('src', 'Images/nike.png');
 			sound.play();
 		});
 
-		$('.nikeT').click(function(){
+		$('.nikeT').mouseenter(function(){
 			$('.shoes').attr('src', 'Images/nike_3.png');
 			sound.play();
 		});
-})
+});
 
 					/* Now, try to create a click interaction on
 						The link ADD TO CART which will add 1 to the cart */
 $(document).ready(function(){
-			var	$clicks = 0;
+				var	$clicks = 0;
 				$(".add, .add_2").click(function(){
 					$clicks++;
 				$(".ajout").html("(" + $clicks + ")");
@@ -36,35 +36,43 @@ $(document).ready(function(){
 		slidesPerView: 4,
 		spaceBetween: 3,
 		paginationClickable: true,
-	})
-});
-				/* Code of click on search
-					which displays a
-					Input */
-	/*	$(document).ready(function(){
-			$("#loupe").click(function(){
-				$("#search").html("<input type='text' placeholder='Search'/>");
-			})
-		})*/
-
-							/* Id of the #frame following the scroll*/
-$(document).ready(function(){
-	$(window).scroll(function(){
-		if ($(window).scrollTop() > 500 && $(window).scrollTop() < 1000)
-		$("#frame").css("position", "absolute").css("top", -624);
 	});
 });
+				/* Code of click on search which displays a Input */
+		$(document).ready(function(){
+			$("#search").click(function(){
+					$("#search").html("<input type='text' placeholder='Search'/>");
+					$("#icon li").css("left", "60%");
+			});
+	});
+
+	$(document).ready(function(){
+		$("#loupe").click(function(){
+				$("#search").html("<a>SEARCH</a>");
+				$("#icon li").css("left", "65%");
+		});
+});
+
+							/* Id of the #frame following the scroll*/
+	$(document).ready(function(){
+		$(window).scroll(function(){
+			if ($(window).scrollTop() > 500 && $(window).scrollTop() < 1000)
+			$("#frame").css("position", "absolute").css("top", -624);
+		});
+	});
 
 								/* Code that changes the heart when you click */
 $(document).ready(function(){
-				$(".heart").click(function(){
-				$(".heart").attr("src", "Images/heart_2.png");
+	$(".heart").click(function(){
+		$(".heart").css("visibility", "hidden");
+		$(".heart_2").css("visibility", "visible");
 	});
 });
 
 $(document).ready(function(){
-				$("heart").click(function(){
-				$(".heart").attr("src", "Images/heart.png");
+	$(".heart_2").click(function(){
+			$(".heart_2").css("visibility", "hidden");
+			$(".heart").css("visibility", "visible");
 		});
 });
 
